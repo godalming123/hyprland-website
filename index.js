f6a6e43cad5d39b5d3bef6833c35a227011e5ee9
godalming123 <r2hk9ahnf@relay.firefox.com>
2023-03-03 06:18:16 +1200

    make final commits

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //register service worker
-
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register("sw.js", { scope: "https://godalming123.github.io/hyprland-website/" })
     .then (reg => {console.info ('Service Worker registration successfull:', reg);})
